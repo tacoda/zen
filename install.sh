92258ef83504a538a4c4620a1993efe0f98faa94
Ian Johnson <tacoda@hey.com>
2025-05-31 17:19:54 +0100

    Add install dummy scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 set -e
 
-echo "Hello world"
+trap 'echo "Zen installation failed! You can retry by running: source ~/.local/share/zen/install.sh"' ERR
+
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+
+echo "Installing terminal and desktop tools..."
+
+# Install terminal tools
+source ~/.local/share/zen/install/terminal.sh
+
+# Install desktop tools and tweaks
+source ~/.local/share/zen/install/desktop.sh
+
+# Revert to normal idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
