94a6ce76aa0aa16ab8321b6fefdcaa670926267a
Ian Johnson <tacoda@hey.com>
2025-05-31 17:57:35 +0100

    Comment ppa for php for now

--- install/terminal/set-dev-languages.sh
+++ install/terminal/set-dev-languages.sh
@@ -6,7 +6,7 @@ mise use --global node@lts
 
 mise use --global go@latest
 
-sudo add-apt-repository -y ppa:ondrej/php
+# sudo add-apt-repository -y ppa:ondrej/php
 sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
