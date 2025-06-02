43ae753b415531cf8432725ef5934ce5d13433bb
Ian Johnson <tacoda@hey.com>
2025-06-02 12:50:30 +0100

    Add libonig to libraries

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -2,4 +2,4 @@ sudo apt install -y \
   build-essential pkg-config autoconf bison clang \
   libcurl4-openssl-dev libgd-dev libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
-  redis-tools sqlite3 libsqlite3-0 libsqlite3-dev libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common re2c
+  libonig-dev redis-tools sqlite3 libsqlite3-0 libsqlite3-dev libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common re2c
