7b5bbaae4a8a089b4d26d9b81fbd9d5b5edea27c
Ian Johnson <tacoda@hey.com>
2025-06-02 12:27:47 +0100

    Add php to mise setup

--- install/terminal/set-dev-languages.sh
+++ install/terminal/set-dev-languages.sh
@@ -6,11 +6,10 @@ mise use --global node@lts
 
 mise use --global go@latest
 
-# sudo add-apt-repository -y ppa:ondrej/php
-# sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
-# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-# php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
-# rm composer-setup.php
+mise use --global php@latest
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
+rm composer-setup.php
 
 mise use --global python@latest
 
