07bdca1ba234e2b7b38d3ef3e50c9ef00fd0fff4
Ian Johnson <tacoda@hey.com>
2025-06-02 12:46:52 +0100

    Add libgd to libraries

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -1,5 +1,5 @@
 sudo apt install -y \
   build-essential pkg-config autoconf bison clang \
-  libcurl4-openssl-dev libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
+  libcurl4-openssl-dev libgd-dev libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
   redis-tools sqlite3 libsqlite3-0 libsqlite3-dev libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common re2c
