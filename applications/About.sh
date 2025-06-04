b621943bc94115dcfb0b76498c3d7e41907672ef
Ian Johnson <tacoda@hey.com>
2025-06-04 12:43:37 +0100

    Add applications and icons

---
+++ applications/About.sh
@@ -0,0 +1,12 @@
+cat <<EOF >~/.local/share/applications/About.desktop
+[Desktop Entry]
+Version=1.0
+Name=About
+Comment=System information from Fastfetch
+Exec=alacritty --config-file /home/$USER/.config/alacritty/pane.toml --class=About --title=About -e bash -c 'fastfetch; read -n 1 -s'
+Terminal=false
+Type=Application
+Icon=/home/$USER/.local/share/zen/applications/icons/Ubuntu.png
+Categories=GTK;
+StartupNotify=false
+EOF
