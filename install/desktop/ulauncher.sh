9ff79dcb7c0d612ae3b94c9bbee64fdddc9958e8
Ian Johnson <tacoda@hey.com>
2025-06-04 12:20:47 +0100

    Fix ulauncher installation

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -1,6 +1,4 @@
-gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
-gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] http://ppa.launchpad.net/agornostal/ulauncher/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/ulauncher-noble.list
+sudo add-apt-repository ppa:agornostal/ulauncher
 
 sudo apt update -y
 sudo apt install -y ulauncher
