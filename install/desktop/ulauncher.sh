a676c58d58e7084e29a6fcda94d36fe19a412b9f
Ian Johnson <tacoda@hey.com>
2025-06-04 12:35:22 +0100

    Comment ulauncher for now

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -1,12 +1,12 @@
-sudo add-apt-repository -y universe
-sudo add-apt-repository -y ppa:agornostal/ulauncher
+# sudo add-apt-repository -y universe
+# sudo add-apt-repository -y ppa:agornostal/ulauncher
 
-sudo apt update -y
-sudo apt install -y ulauncher
+# sudo apt update -y
+# sudo apt install -y ulauncher
 
 # Start ulauncher to have it populate config before we overwrite
-mkdir -p ~/.config/autostart/
-cp ~/.local/share/zen/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
-gtk-launch ulauncher.desktop >/dev/null 2>&1
-sleep 2 # ensure enough time for ulauncher to set defaults
-cp ~/.local/share/zen/configs/ulauncher.json ~/.config/ulauncher/settings.json
+# mkdir -p ~/.config/autostart/
+# cp ~/.local/share/zen/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
+# gtk-launch ulauncher.desktop >/dev/null 2>&1
+# sleep 2 # ensure enough time for ulauncher to set defaults
+# cp ~/.local/share/zen/configs/ulauncher.json ~/.config/ulauncher/settings.json
