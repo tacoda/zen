aba983b0b2a6216fffedf54c323f62faa75c00fc
Ian Johnson <tacoda@hey.com>
2025-06-05 13:11:10 +0100

    Toggle error flag

--- install/identification.sh
+++ install/identification.sh
@@ -1,5 +1,6 @@
 echo "Enter identification for git and autocomplete..."
 SYSTEM_NAME=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
+set +e
 echo -n "Personal Name> "
 read -r personal_name
 echo -n "Personal Email> "
@@ -12,3 +13,4 @@ export ZEN_USER_NAME="$personal_name"
 export ZEN_USER_EMAIL="$personal_email"
 # export ZEN_WORK_USER_NAME=$work_name
 # export ZEN_WORK_USER_EMAIL=$work_email
+set -e
