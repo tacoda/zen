a31f0d34eee738cde70c46af60949781605d6394
Ian Johnson <tacoda@hey.com>
2025-06-05 12:48:22 +0100

    Add kubectl, kubectx, k9s

---
+++ install/terminal/app-k9s.sh
@@ -0,0 +1,3 @@
+wget https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_linux_arm64.deb
+sudo apt install -y ./k9s_linux_arm64.deb
+rm k9s_linux_arm64.deb
---
+++ install/terminal/app-kubectl.sh
@@ -0,0 +1,3 @@
+sudo apt update -y && sudo apt install -y gpg apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
