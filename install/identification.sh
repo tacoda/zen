db5100c34dedf251bd5fbd5ae55e78daba88a142
Ian Johnson <tacoda@hey.com>
2025-06-05 13:02:56 +0100

    Update name and email input

--- install/identification.sh
+++ install/identification.sh
@@ -1,7 +1,8 @@
 echo "Enter identification for git and autocomplete..."
 SYSTEM_NAME=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
-export ZEN_USER_NAME="Ian Johnson"
-export ZEN_USER_EMAIL="tacoda@hey.com"
-
-# export ZEN_USER_NAME=$(gum input --placeholder "Enter full name" --value "$SYSTEM_NAME" --prompt "Name> ")
-# export ZEN_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+echo "Name> "
+read -r name
+echo "Email> "
+read -r email
+export ZEN_USER_NAME=$name
+export ZEN_USER_EMAIL=$email
