f4999e481273dbeffe0d0975ad2806a44bcc3130
Ian Johnson <tacoda@hey.com>
2025-06-05 13:12:49 +0100

    Revert name and email

--- install/identification.sh
+++ install/identification.sh
@@ -1,16 +1,6 @@
 echo "Enter identification for git and autocomplete..."
 SYSTEM_NAME=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
-set +e
-echo -n "Personal Name> "
-read -r personal_name
-echo -n "Personal Email> "
-read -r personal_email
-# echo "Work Name> "
-# read -r work_name
-# echo "Work Email> "
-# read -r work_email
-export ZEN_USER_NAME="$personal_name"
-export ZEN_USER_EMAIL="$personal_email"
+export ZEN_USER_NAME="Ian Johnson"
+export ZEN_USER_EMAIL="tacoda@hey.com"
 # export ZEN_WORK_USER_NAME=$work_name
 # export ZEN_WORK_USER_EMAIL=$work_email
-set -e
