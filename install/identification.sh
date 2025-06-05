ad581cd22742ce12987c9468052f48a856e28d12
Ian Johnson <tacoda@hey.com>
2025-06-05 13:09:24 +0100

    Fix user input

--- install/identification.sh
+++ install/identification.sh
@@ -1,14 +1,14 @@
 echo "Enter identification for git and autocomplete..."
 SYSTEM_NAME=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
-echo "Personal Name> "
+echo -n "Personal Name> "
 read -r personal_name
-echo "Personal Email> "
+echo -n "Personal Email> "
 read -r personal_email
 # echo "Work Name> "
 # read -r work_name
 # echo "Work Email> "
 # read -r work_email
-export ZEN_USER_NAME=$personal_name
-export ZEN_USER_EMAIL=$personal_email
+export ZEN_USER_NAME="$personal_name"
+export ZEN_USER_EMAIL="$personal_email"
 # export ZEN_WORK_USER_NAME=$work_name
 # export ZEN_WORK_USER_EMAIL=$work_email
