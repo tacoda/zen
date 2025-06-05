3c6f776ff1e01a57262c6b43808099d4d8c7c840
Ian Johnson <tacoda@hey.com>
2025-06-05 13:06:07 +0100

    Update name and email input again

--- install/identification.sh
+++ install/identification.sh
@@ -1,8 +1,14 @@
 echo "Enter identification for git and autocomplete..."
 SYSTEM_NAME=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
-echo "Name> "
-read -r name
-echo "Email> "
-read -r email
-export ZEN_USER_NAME=$name
-export ZEN_USER_EMAIL=$email
+echo "Personal Name> "
+read -r personal_name
+echo "Personal Email> "
+read -r personal_email
+# echo "Work Name> "
+# read -r work_name
+# echo "Work Email> "
+# read -r work_email
+export ZEN_USER_NAME=$personal_name
+export ZEN_USER_EMAIL=$personal_email
+# export ZEN_WORK_USER_NAME=$work_name
+# export ZEN_WORK_USER_EMAIL=$work_email
