25fc5306bfd517e60d548c8e22567b17bbe28343
Ian Johnson <tacoda@hey.com>
2025-06-06 14:07:38 +0100

    Fix lazysql installation

--- install/terminal/app-lazysql.sh
+++ install/terminal/app-lazysql.sh
@@ -1,6 +1,6 @@
 cd /tmp
 LAZYSQL_VERSION=$(curl -s "https://api.github.com/repos/jorgerojas26/lazysql/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -sLo lazydocker.tar.gz "https://github.com/jorgerojas26/lazysql/releases/download/${LAZYSQL_VERSION}/lazysql_Linux_arm64.tar.gz"
+curl -sLo lazysql.tar.gz "https://github.com/jorgerojas26/lazysql/releases/download/${LAZYSQL_VERSION}/lazysql_Linux_arm64.tar.gz"
 tar -xf lazysql.tar.gz lazysql
 sudo install lazysql /usr/local/bin
 rm lazysql.tar.gz lazysql
