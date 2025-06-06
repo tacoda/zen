0987d8ef75a1c8ef10f92b398f698287d0db81c4
Ian Johnson <tacoda@hey.com>
2025-06-06 13:56:30 +0100

    Add lazysql and bruno

---
+++ install/terminal/app-bruno.sh
@@ -0,0 +1,7 @@
+sudo mkdir -p /etc/apt/keyrings
+sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/bruno.gpg --keyserver keyserver.ubuntu.com --recv-keys 9FA6017ECABE0266
+
+echo "deb [signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" | sudo tee /etc/apt/sources.list.d/bruno.list
+
+sudo apt -y update
+sudo apt install -y bruno
