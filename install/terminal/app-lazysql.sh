e48219de13dadaf051425f600519121a17f84d97
Ian Johnson <tacoda@hey.com>
2025-06-06 14:12:45 +0100

    Comment lazsql for now

--- install/terminal/app-lazysql.sh
+++ install/terminal/app-lazysql.sh
@@ -1,7 +1,7 @@
-cd /tmp
-LAZYSQL_VERSION=$(curl -s "https://api.github.com/repos/jorgerojas26/lazysql/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -sLo lazysql.tar.gz "https://github.com/jorgerojas26/lazysql/releases/download/${LAZYSQL_VERSION}/lazysql_Linux_arm64.tar.gz"
-tar -xf lazysql.tar.gz lazysql
-sudo install lazysql /usr/local/bin
-rm lazysql.tar.gz lazysql
-cd -
+# cd /tmp
+# LAZYSQL_VERSION=$(curl -s "https://api.github.com/repos/jorgerojas26/lazysql/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# curl -sLo lazysql.tar.gz "https://github.com/jorgerojas26/lazysql/releases/download/${LAZYSQL_VERSION}/lazysql_Linux_arm64.tar.gz"
+# tar -xf lazysql.tar.gz lazysql
+# sudo install lazysql /usr/local/bin
+# rm lazysql.tar.gz lazysql
+# cd -
