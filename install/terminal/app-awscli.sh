5aa59738be59158c118ff4c8caaa7456b9ad2e16
Ian Johnson <tacoda@hey.com>
2025-06-06 17:26:29 +0100

    Automatically update aws cli if it is already installed

--- install/terminal/app-awscli.sh
+++ install/terminal/app-awscli.sh
@@ -1,4 +1,10 @@
-if ! command -v aws >/dev/null 2>&1; then
+if command -v aws >/dev/null 2>&1; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" --output "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+  rm awscliv2.zip
+  rm -rf aws
+else
   curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" --output "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install
