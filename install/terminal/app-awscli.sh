d48a96438b59c9d7307f93b6720555f5ce22981b
Ian Johnson <tacoda@hey.com>
2025-06-06 17:22:15 +0100

    Add condition for installing aws cli

--- install/terminal/app-awscli.sh
+++ install/terminal/app-awscli.sh
@@ -1,6 +1,7 @@
-# TODO: First check if aws is installed
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" --output "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-# rm awscliv2.zip
-# rm -rf aws
+if ! command -v aws >/dev/null 2>&1; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" --output "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  rm awscliv2.zip
+  rm -rf aws
+fi
