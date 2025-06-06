9894bd9808728808aa84aebdfda9a4f3d34b16c9
Ian Johnson <tacoda@hey.com>
2025-06-06 13:45:15 +0100

    Reenable aws cli

--- install/terminal/app-awscli.sh
+++ install/terminal/app-awscli.sh
@@ -1,5 +1,5 @@
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-# rm awscliv2.zip
-# rm -rf aws
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" --output "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+rm -rf aws
