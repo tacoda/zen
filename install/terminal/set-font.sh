27aac222a3278b52c354548dcad4f1018680d8d3
Ian Johnson <tacoda@hey.com>
2025-06-06 15:21:59 +0100

    Add fira code font

---
+++ install/terminal/set-font.sh
@@ -0,0 +1,24 @@
+set_font() {
+  local font_name=$1
+  local url=$2
+  local file_type=$3
+  local file_name="${font_name/ Nerd Font/}"
+
+  if ! $(fc-list | grep -i "$font_name" >/dev/null); then
+    cd /tmp
+    wget -O "$file_name.zip" "$url"
+    unzip "$file_name.zip" -d "$file_name"
+    cp "$file_name"/*."$file_type" ~/.local/share/fonts
+    rm -rf "$file_name.zip" "$file_name"
+    fc-cache
+    cd -
+    # clear
+    # source $OMAKUB_PATH/ascii.sh
+  fi
+
+  gsettings set org.gnome.desktop.interface monospace-font-name "$font_name 16"
+  cp "$ZEN_PATH/configs/alacritty/fonts/$file_name.toml" ~/.config/alacritty/font.toml
+  # sed -i "s/\"editor.fontFamily\": \".*\"/\"editor.fontFamily\": \"$font_name\"/g" ~/.config/Code/User/settings.json
+}
+
+set_font "FiraMono Nerd Font" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraMono.zip" "otf"
