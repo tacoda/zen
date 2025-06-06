68af68aa05ecdb2b290b05d5349269e4793217d9
Ian Johnson <tacoda@hey.com>
2025-06-06 16:13:26 +0100

    Add update script

---
+++ bin/update.sh
@@ -0,0 +1,8 @@
+sudo apt-get update && sudo apt-get -y upgrade
+source $ZEN_PATH/install/terminal/app-lazygit.sh && gum spin --spinner globe --title "Lazygit updated!" -- sleep 3
+source $ZEN_PATH/install/terminal/app-lazydocker.sh && gum spin --spinner globe --title "Lazydocker updated!" -- sleep 3
+source $ZEN_PATH/install/terminal/app-lazysql.sh && gum spin --spinner globe --title "Lazsql updated!" -- sleep 3
+source $ZEN_PATH/install/terminal/app-k9s.sh && gum spin --spinner globe --title "k9s updated!" -- sleep 3
+source $ZEN_PATH/install/terminal/app-neovim.sh && gum spin --spinner globe --title "Neovim updated!" -- sleep 3
+source $ZEN_PATH/install/terminal/app-zellij.sh && gum spin --spinner globe --title "Zellij updated!" -- sleep 3
+source $ZEN_PATH/install/terminal/app-github-cli.sh && gum spin --spinner globe --title "GitHub cli updated!" -- sleep 3
