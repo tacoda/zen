7361da886a1c8716b8539fb22f1c3d70f529522a
Ian Johnson <tacoda@hey.com>
2025-06-09 18:40:37 +0100

    Fix kubectl installation

--- install/terminal/app-kubectl.sh
+++ install/terminal/app-kubectl.sh
@@ -1,3 +1,7 @@
 sudo apt update -y && sudo apt install -y gpg apt-transport-https ca-certificates curl gnupg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubectl
